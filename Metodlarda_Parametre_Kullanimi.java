db6d0f78f974d11a717cde52caa3b710f71c91b4
Alinur Kaan Baytan Erdoğan <57916206+Alinurkaanbaytanerdogan@users.noreply.github.com>
2020-02-11 20:35:48 +0300

    Metodlar 2

---
+++ Metodlarda_Parametre_Kullanimi.java
@@ -0,0 +1,21 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class Metodlarda_Parametre_Kullanimi {
+	
+	public static void selamlama(String isim) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Ismin ne: ");
+		isim = scanner.next();
+		System.out.println("Selam, " + isim);
+		
+	}
+	public static void main(String[] args) {
+		String isim = "";
+		selamlama(isim);
+		selamlama(isim);
+		
+	}
+
+}

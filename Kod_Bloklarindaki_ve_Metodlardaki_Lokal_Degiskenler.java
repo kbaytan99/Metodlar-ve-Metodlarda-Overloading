824d271bb3a8f94f2612379569fc500602cd3ed2
Alinur Kaan Baytan Erdoğan <57916206+Alinurkaanbaytanerdogan@users.noreply.github.com>
2020-02-11 21:34:14 +0300

    Methodlar 5

---
+++ Kod_Bloklarindaki_ve_Metodlardaki_Lokal_Degiskenler.java
@@ -0,0 +1,70 @@
+package Methods;
+
+public class Kod_Bloklarindaki_ve_Metodlardaki_Lokal_Degiskenler {
+
+	public static void main(String[] args) {
+		
+	//	 int a = 4;
+		// String yaz = "Selam";
+		 
+		 //if(a < 10) {
+			// System.out.println(yaz);
+			 
+			
+	//		for(int i = 0; i <= 10; i++) {
+	//			System.out.println(i);
+//			}
+			//KOD BLOKLARI DISINDA BASKA BISEY KULLANAMAZSINIZ O BLOGUN ICINDE TEK KULLANILIR
+		
+			int i = 0;
+			
+			while(i <= 10) {
+				i++;
+				switch(i) {
+				case 1:
+				System.out.println(i + ": " + "Kaan");
+				break;
+				
+				case 2: 
+				System.out.println(i + ": " + "Deneme");
+				break;
+				
+				case 3:
+				System.out.println(i + ": " + "Sert");
+				break;
+				
+				case 4:
+				System.out.println(i + ": " + "Sert");
+				break;
+				
+				case 5:
+				System.out.println(i + ": " + "Sertoc");
+				break;
+				
+				case 6:
+				System.out.println(i + ": " + "Sertoc");
+				break;	
+				
+				case 7:
+				System.out.println(i + ": " + "Sertoc");
+				break;
+				
+				case 8:
+				System.out.println(i + ": " + "Sertoc");
+				break;
+				
+				case 9:
+				System.out.println(i + ": " + "Sertoc");
+				break;
+				
+				case 10:
+				System.out.println(i + ": " + "Sertoc");
+				break;
+				
+				}
+			}
+			
+			
+		}
+	}
+//}
